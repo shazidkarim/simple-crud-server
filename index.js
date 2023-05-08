2dc831bc7e5c81568c9ec8d3d690022ad5528816
shazidkarim <shazid747@gmail.com>
2023-05-08 23:17:01 +0600

    user delete created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,6 +30,12 @@ async function run() {
     const database = client.db("usersDB");
     const userCollection = database.collection("users");
 
+    app.get('/users', async (req,res)=>{
+      const cursor = userCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     app.post('/users', async (req,res)=>{
          const user = req.body;
          console.log('new user:' , user);
@@ -37,6 +43,13 @@ async function run() {
          res.send(result);
     })
 
+    app.delete('/users/:id',async(req,res)=>{
+      const id = req.params.id;
+      console.log('please delete id from database:',id);
+      const query = { _id: new ObjectId(id)  }
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
