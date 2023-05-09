b3965a2ea65c5f4e2bb6bff51d6ab1e763b6cfb3
shazidkarim <shazid747@gmail.com>
2023-05-09 17:02:06 +0600

    user update created

--- index.js
+++ index.js
@@ -36,11 +36,35 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const user = await userCollection.findOne(query)
+      res.send(user);
+    })
+
+
+
     app.post('/users', async (req,res)=>{
          const user = req.body;
          console.log('new user:' , user);
          const result = await userCollection.insertOne(user);
          res.send(result);
+    })
+    app.put('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const user = req.body;
+      console.log(user);
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert:true}
+      const updatedUser = {
+        $set:{
+          name: user.name,
+          email:user.email
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedUser,options);
+      res.send(result);
     })
 
     app.delete('/users/:id',async(req,res)=>{
